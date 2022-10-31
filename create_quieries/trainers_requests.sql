1a8a2e2c523c70e3ac9e9a6949d8dde76d42b926
Artem <90412503+artemmoroz0v@users.noreply.github.com>
2022-10-31 23:45:24 +0300

    Add files via upload

---
+++ create_quieries/trainers_requests.sql
@@ -0,0 +1,11 @@
+INSERT INTO "Trainer" (trainer_id, trainer_second_name, trainer_name, trainer_third_name, trainer_phone) VALUES
+(0, 'Kodzhaspirov', 'Yuriy', 'Georgievich', '+79539421212'),
+(1, 'Gorbunov', 'Arkadiy', 'Vyacheslavovich', '+79321234431'),
+(2, 'Zhitnyakov', 'Mikhail', 'Vasilievich', '+79121212211'),
+(3, 'Afanasyev', 'Petr', 'Ivanovich', '+79321213344'),
+(4, 'Petrov', 'Ivan', 'Artemovich', '+79231213455'),
+(5, 'Serebrennikov', 'Mikhail', 'Petrovich', '+79208881122'),
+(6, 'Artemov', 'Ivan', 'Ivanovich', '+79134441212'),
+(7, 'Ivanov', 'Yuriy', 'Artemovich', '+79201112324'),
+(8, 'Trofimov', 'Valeriy', 'Ivanovich', '+79204431243'),
+(9, 'Pavlikov', 'Nikita', 'Petrovich', '+79120442312');
