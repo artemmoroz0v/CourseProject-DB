4dcec152bed5b664d1c8e717a417abe053e0e9d0
Матвей Попов <evgeniyprohorov24@gmail.com>
2022-11-13 17:51:24 +0300

    Update struct of the project

--- BackAPI/internal/structs/group_struct.go
+++
@@ -1,9 +0,0 @@
-package structs
-
-type Group struct {
-	GroupID       int    `json:"groupID"`
-	ProgramID     int    `json:"programID"`
-	Notes         string `json:"notes"`
-	TrainerID     int    `json:"trainerID"`
-	ClientsAmount int    `json:"clientsAmount"`
-}
--- BackAPI/internal/structs/trainer_struct.go
+++
@@ -1,9 +0,0 @@
-package structs
-
-type Trainer struct {
-	TrainerID         int    `json:"trainerID"`
-	TrainerSecondName string `json:"trainerSecondName"`
-	TrainerName       string `json:"trainerName"`
-	TrainerThirdName  string `json:"trainerThirdName"`
-	TrainerPhone      string `json:"trainerPhone"`
-}
