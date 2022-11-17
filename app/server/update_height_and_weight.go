ee95153bd7075d356a76dcd2fe7c46727e8898de
Artem <90412503+artemmoroz0v@users.noreply.github.com>
2022-11-17 23:35:01 +0300

    Update update_height_and_weight.go

--- app/server/update_height_and_weight.go
+++ app/server/update_height_and_weight.go
@@ -5,12 +5,25 @@ import (
 )
 
 func UpdateHeightAndWeight(id int, height int, weight int) error {
-	if height == 0 {
+	var err error
+	if height == 0 && weight != 0 {
+		_, err = db.Exec(`
+		UPDATE Client
+		SET weight = $1 WHERE
+		(subscription_id = $2)`,
+			weight, id)
 		return errors.New("error: zero-value for height had been inputed")
-	} else if weight == 0 {
+	} else if weight == 0 && height != 0 {
+		_, err = db.Exec(`
+		UPDATE Client
+		SET height = $1 WHERE
+		(subscription_id = $2)`,
+			height, id)
 		return errors.New("error: zero-value for weight had been inputed")
+	} else if height == 0 && weight == 0 {
+		return errors.New("error: zero-values for weight and height had been inputed")
 	}
-	_, err := db.Exec(`
+	_, err = db.Exec(`
 		UPDATE Client
 		SET height = $1,
 		weight = $2 WHERE
